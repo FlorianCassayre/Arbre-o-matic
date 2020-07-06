2070b9079b2b54edc1e31df8f6fd41594fbf3d32
Florian Cassayre <florian.cassayre@gmail.com>
2020-07-06 12:09:44 +0200

    Installation script.

---
+++ update.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Exit on failure
+set -e
+
+# Set current working directory
+cd "$(dirname "$0")"
+
+# Pull changes (if any)
+git pull
+
+# Install npm packages
+npm install
+
+# Clean build directory
+rm -rf dist
+
+# Build production artifact
+npm run prod
+
+# Set newly built artifact to production
+rm -rf production
+mv dist production
